1f1cf26da2b6b02166c7c5c22b83cfdc1c83864d
ThaiMing3 <tanthaiming@gmail.com>
2023-12-18 17:27:09 +0800

    Remove import

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import 'react-native-get-random-values';
 import {AppRegistry} from 'react-native';
 import AppWrapper from './App';
 import {name as appName} from './app.json';
-import PushNotification from 'react-native-push-notification';
-import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 LogBox.ignoreLogs([
   /Invalid query/, // Use a regular expression to match part of the warning message
